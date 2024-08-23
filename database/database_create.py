03dea7ca095fc5884d68e47468e6dee937293fac
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2024-08-23 18:42:10 -0300

    feat: add structure for create table

--- database/database_create.py
+++ database/database_create.py
@@ -9,4 +9,23 @@ class Connect:
 
   def create_tb_user(self) -> None:
     
-    self.cursor.execute()
+    self.cursor.execute(
+      "CREATE TABLE IF NOT EXISTS TB_BOOK ( \
+        BOOK_ID,\
+        BOOK_TITLE,\
+        BOOK_AUTHOR,\
+        BOOK_PUBCOMPANY,\
+        BOOK_GENDER,\
+        BOOK_AMOUNT )\
+      ")
+    
+  def create_tb_student(self) -> None:
+
+      self.cursor.execute(
+      "CREATE TABLE IF NOT EXISTS TB_USER ( \
+        USER_ID,\
+        USER_NAME,\
+        USER_PHONE,\
+        USER_ADDRESS,\
+        )\
+      ")
